7750f77b87a520b2c438c818ad9d316a4b1a80f2
jogpfls <joerin4177@gmail.com>
2025-11-03 20:20:26 +0900

    init: prettier, eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,49 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import react from "eslint-plugin-react";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ["node_modules", "dist"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      react,
+      "simple-import-sort": simpleImportSort,
+    },
+    settings: { react: { version: "detect" } },
+    rules: {
+      curly: ["error", "all"],
+      "no-console": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_|^args$" },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^node:"],
+            ["^react$", "^@?\\w"],
+            ["^@pages/*", "^@shared/*"],
+            ["^\\u0000"],
+            ["^\\."],
+            ["^.+\\.css$"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
     },
   },
-])
+  prettierConfig,
+);
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 80,
+};
