51245f5231ae74c3ad1a6c71915aec5880428c94
jogpfls <joerin4177@gmail.com>
2025-11-06 21:01:36 +0900

    init: color, typography style tokens 추가

---
+++ src/shared/styles/token/font-style.css.ts
@@ -0,0 +1,73 @@
+import { typography } from './typography.css';
+
+export const fontStyles = {
+  //Heavy
+  head_hv_80: {
+    fontSize: typography.fontSize[80],
+    fontWeight: typography.fontWeight.heavy,
+  },
+  head_hv_45: {
+    fontSize: typography.fontSize[45],
+    fontWeight: typography.fontWeight.heavy,
+  },
+  head_hv_35: {
+    fontSize: typography.fontSize[35],
+    fontWeight: typography.fontWeight.heavy,
+  },
+  head_hv_20: {
+    fontSize: typography.fontSize[20],
+    fontWeight: typography.fontWeight.heavy,
+  },
+
+  // Extra Bold
+  title_eb_25: {
+    fonnSize: typography.fontSize[25],
+    fontWeight: typography.fontWeight.extrabold,
+  },
+  title_eb_30: {
+    fonnSize: typography.fontSize[30],
+    fontWeight: typography.fontWeight.extrabold,
+  },
+
+  // Bold
+  body_b_14: {
+    fontSize: typography.fontSize[14],
+    fontWeight: typography.fontWeight.bold,
+  },
+  body_b_18: {
+    fontSize: typography.fontSize[18],
+    fontWeight: typography.fontWeight.bold,
+  },
+  body_b_20: {
+    fontSize: typography.fontSize[20],
+    fontWeight: typography.fontWeight.bold,
+  },
+  body_b_30: {
+    fontSize: typography.fontSize[30],
+    fontWeight: typography.fontWeight.bold,
+  },
+
+  // Semi Bold
+  body_sb_12: {
+    fontSize: typography.fontSize[12],
+    fontWeight: typography.fontWeight.semibold,
+  },
+
+  // Regular
+  section_r_10: {
+    fontSize: typography.fontSize[10],
+    fontWeight: typography.fontWeight.regular,
+  },
+  section_r_12: {
+    fontSize: typography.fontSize[12],
+    fontWeight: typography.fontWeight.regular,
+  },
+  section_r_14: {
+    fontSize: typography.fontSize[14],
+    fontWeight: typography.fontWeight.regular,
+  },
+  section_r_16: {
+    fontSize: typography.fontSize[16],
+    fontWeight: typography.fontWeight.regular,
+  },
+};
