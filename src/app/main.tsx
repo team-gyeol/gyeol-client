9a99c0cdd53d911dd7afb2ee3acfde9cb01c09cd
jogpfls <joerin4177@gmail.com>
2025-11-07 01:01:14 +0900

    init: 폴더 구조 설정

---
+++ src/app/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRoot } from "react-dom/client";
+
+import ThemeProvider from "@shared/styles/theme-provider";
+import { queryClient } from "@shared/utils/query-client.ts";
+
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>,
+);
