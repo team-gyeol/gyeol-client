a1b423d08ebe3761b7b5b5c13bb4f747626343e5
jogpfls <joerin4177@gmail.com>
2025-11-06 21:23:46 +0900

    feat: tanstack query 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from './shared/utils/query-client.ts';
 
-createRoot(document.getElementById("root")!).render(
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
