7fd43e9e1e5912d63c609e674240221a4b2ec45a
jogpfls <joerin4177@gmail.com>
2025-11-17 02:41:55 +0900

    feat: home 구현

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,56 @@
+import { useEffect, useRef, useState } from "react";
+import { routePath } from "@router/path";
+import { useNavigate } from "react-router";
+
+import Layout from "@shared/components/layout/layout";
+
+import Accuracy from "./components/accuracy/accuracy";
+import Labeling from "./components/labeling/labeling";
+import Start from "./components/start/start";
+import Team from "./components/team/team";
+
 const Home = () => {
-  return <div>home-page</div>;
+  const [darkHeader, setDarkHeader] = useState(false);
+  const labelingRef = useRef<HTMLDivElement | null>(null);
+  const accuracyRef = useRef<HTMLDivElement | null>(null);
+  const teamRef = useRef<HTMLDivElement | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const targets = [
+      labelingRef.current,
+      accuracyRef.current,
+      teamRef.current,
+    ].filter((el): el is HTMLDivElement => el !== null);
+
+    if (targets.length === 0) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        // 3개 섹션 중 하나라도 화면에 절반 이상 보이면 darkHeader = true
+        const isDarkSectionVisible = entries.some(
+          (entry) => entry.isIntersecting,
+        );
+        setDarkHeader(isDarkSectionVisible);
+      },
+      { threshold: 0.5 },
+    );
+
+    targets.forEach((target) => observer.observe(target));
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <>
+      <Layout darkHeader={darkHeader}>
+        <Start onClick={() => navigate(routePath.UPLOAD)} />
+        <Labeling ref={labelingRef} />
+        <Accuracy ref={accuracyRef} />
+        <Team ref={teamRef} />
+      </Layout>
+    </>
+  );
 };
 
 export default Home;
