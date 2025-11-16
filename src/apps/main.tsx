2df38781aeca493c731f9b8c2f94d8557c2edc04
jogpfls <joerin4177@gmail.com>
2025-11-17 02:05:47 +0900

    fix: browserRouter 설정 추가

--- src/apps/main.tsx
+++ src/apps/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
 import ThemeProvider from "@shared/styles/theme-provider";
 import { queryClient } from "@shared/utils/query-client";
@@ -12,7 +13,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
