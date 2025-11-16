1666c3acbd75998abaffcb53663c27d89570f1c7
jogpfls <joerin4177@gmail.com>
2025-11-17 02:06:27 +0900

    feat: 라우터 path 추가

--- src/router/lazy.ts
+++ src/router/lazy.ts
@@ -1,3 +1,5 @@
 import { lazy } from "react";
 
 export const HomePage = lazy(() => import("@pages/home/home"));
+export const LoginPage = lazy(() => import("@pages/login/login"));
+export const UploadPage = lazy(() => import("@pages/upload/upload"));
--- src/router/path.ts
+++ src/router/path.ts
@@ -1,6 +1,8 @@
 export const routePath = {
   LAYOUT: "/",
   ROOT: "/",
+  LOGIN: "/auth/login",
+  UPLOAD: "/upload",
 } as const;
 
 export type Routes = (typeof routePath)[keyof typeof routePath];
