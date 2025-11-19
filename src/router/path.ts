c1708f5cbb1f76e8725cf0e8e8dfa26a1f460737
jogpfls <joerin4177@gmail.com>
2025-11-20 03:47:41 +0900

    chore: router 관련 코드 수정

--- src/router/path.ts
+++ src/router/path.ts
@@ -4,6 +4,8 @@ export const routePath = {
   LOGIN: "/auth/login",
   UPLOAD: "/upload",
   MY: "/my",
+
+  LOGIN_FALLBACK: "/login-fallback",
 } as const;
 
 export type Routes = (typeof routePath)[keyof typeof routePath];
