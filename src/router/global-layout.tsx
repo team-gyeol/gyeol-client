007561ecf8f49dc2afae1680d46bfed7fc6077be
jogpfls <joerin4177@gmail.com>
2025-11-20 14:44:11 +0900

    feat: scrollToTop 구현

--- src/router/global-layout.tsx
+++ src/router/global-layout.tsx
@@ -1,10 +1,15 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router";
 
+import ScrollToTop from "./scroll-to-top";
+
 export default function GlobalLayout() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Outlet />
-    </Suspense>
+    <>
+      <ScrollToTop />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </>
   );
 }
