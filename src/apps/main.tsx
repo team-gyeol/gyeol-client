c28f61f6106b25ccc5844088ed84269a0c35026d
jogpfls <joerin4177@gmail.com>
2025-11-25 01:57:59 +0900

    fix: 최종 수정

--- src/apps/main.tsx
+++ src/apps/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { router } from "@router/router.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
@@ -18,7 +18,7 @@ createRoot(document.getElementById("root")!).render(
         <RouterProvider router={router} />
         <App />
       </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </StrictMode>,
 );
